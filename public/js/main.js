f315e944240bd1b8685b01efde479f49e33dff63
B-c0des <83431578+B-c0des@users.noreply.github.com>
2021-06-01 18:38:49 -0700

    6-1-2021

--- public/js/main.js
+++ public/js/main.js
@@ -56,7 +56,10 @@ let output = document.getElementById('output');
         document.getElementById('feed').style.display= 'none'
         }
         }
-
+        document.getElementById('phone').addEventListener('input', function (e) {
+          var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+          e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+        });
         function toggleContent1() {
           
           let singleUser = document.querySelectorAll("#img-single-user");
