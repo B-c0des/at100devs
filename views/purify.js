2185a3bc2d06d819ac1f8d77d5d5e300784c3ac1
Bryan Reed <brian.reed.software@outlook.com>
2021-10-07 18:42:43 -0700

    formatted code w/ beautify

--- views/purify.js
+++
@@ -1,9 +0,0 @@
-const purify = require("purify-css")
-const htmlFiles = ['views\index.ejs'];
-const cssFiles = ['public\css\style.css'];
-const opts = {
-    output: 'purified.css'
-};
-purify(htmlFiles, cssFiles, opts, function (res) {
-    log(res);
-});
